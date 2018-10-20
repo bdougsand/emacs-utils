e50018b4eaf0411e2da1f02b1a10aec4f1d0ca30
Brian Sanders <bdougsand+cfb@gmail.com>
2018-10-20 14:39:13 -0400

    Fix to bds/eyebrowse--make-with-window-maximized

--- bds.el
+++ bds.el
@@ -20,7 +20,7 @@
   (let* ((window-configs (eyebrowse--get 'window-configs))
          (new-index (1+ (length window-configs))))
     (eyebrowse-switch-to-window-config new-index)
-    (spacemacs/toggle-maximize-buffer)))
+    (delete-other-windows)))
 
 (defun bds/eyebrowse--open-maximized ()
   (interactive)
